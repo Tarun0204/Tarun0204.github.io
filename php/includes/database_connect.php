2502f0add7df351376a4e268ebcada26122cce79
Tarun0204 <mr.tarun0402@gmail.com>
2023-09-28 15:28:38 +0530

    OK
    UPDATED

---
+++ php/includes/database_connect.php
@@ -0,0 +1,3 @@
+<?php
+    $con=mysqli_connect("localhost","root","","pg life") or die(myslq_error());
+?>
---
+++ php/includes/footer.php
@@ -0,0 +1,21 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bangalore">PG in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2020 Copyright PG Life </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
